191071a46e8c43ae037a3f724187152aa98ff10f
J0K3RS-L4UGH <95757914+J0K3RS-L4UGH@users.noreply.github.com>
2022-10-25 20:21:03 +0100

    Add files via upload

---
+++ paycreate.py
@@ -0,0 +1,8 @@
+wordlist = input('What wordlist do you want to use?: ')
+payload = input('Waht do you want the payload to be called?: ')
+
+with open(wordlist) as inf, open(payload, 'w') as outf:
+    outf.write(next(inf))
+    for line in inf:
+        outf.write("ENTER\nDELAY 0.5\n")
+        outf.write(line)
