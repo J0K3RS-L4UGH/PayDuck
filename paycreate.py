70f1c1c471bfdfe8aa91cc33d5957415337fb21f
J0K3RS-L4UGH <95757914+J0K3RS-L4UGH@users.noreply.github.com>
2022-10-26 12:05:31 +0100

    Update paycreate.py

--- paycreate.py
+++ paycreate.py
@@ -1,8 +1,12 @@
 wordlist = input('What wordlist do you want to use?: ')
 payload = input('Waht do you want the payload to be called?: ')
 
-with open(wordlist) as inf, open(payload, 'w') as outf:
-    outf.write('STRING ', next(inf))
+with open(payload, 'w') as firstr:
+    firstr.write('STRING ')
+
+
+with open(wordlist) as inf, open(payload, 'a') as outf:
+    outf.write(next(inf))
     for line in inf:
         outf.write("ENTER\nDELAY 0.5\n")
-        outf.write(line)
+        outf.write('STRING ' + line)
